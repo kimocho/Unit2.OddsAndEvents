dfcda797a159569b222ed113191c9bebce98d6be
Samuel Kim <samjkim455@gmail.com>
2025-05-30 00:05:54 -0400

    unable to get capture value from input and addeventlistener seems unresponsive on my buttons

--- index.js
+++ index.js
@@ -6,102 +6,82 @@ const state = {
 
 const sec = document.createElement('section');
 const lowerPart = document.createElement('section');
-
 sec.appendChild(lowerPart);
-const miniHeader1 = document.createElement('h2');
-miniHeader1.innerHTML = `
-  <h2>Bank</h2>
-  ${state.bank}
-`;
-lowerPart.appendChild(miniHeader1);
 
 //form onlyyyy
 const userInputForm = document.createElement('form');
 sec.appendChild(userInputForm);
-const inp = document.createElement('input');
-userInputForm.appendChild(inp);
-const inputValue = inp.value;
-console.log(inputValue);
+const myInput = document.createElement('input');
+const inputValue = myInput.value;
+userInputForm.appendChild(myInput);
+
+
 //add a number button
 const addNumberButton = document.createElement('button');
 addNumberButton.innerText = 'Add number';
 userInputForm.appendChild(addNumberButton);
-//what happens if you click add a number button
-addNumberButton.addEventListener('click', (ev) => {
-  ev.preventdefault();
-  console.log(inputValue);
-  state.bank.push(inputValue);
 
-  miniHeader1.innerHTML = `
-    <h2>Bank</h2>
-    ${state.bank}
-  `;
-});
 //form onlyyy
+addNumberButton.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('did it work?');
+  state.bank.push(inputValue);
+  console.log(state.bank);
+});
 
-//sort 1 button
+
+
+
+
+//sort 1 button!!
 const sort1Button = document.createElement('button');
 sort1Button.innerText = 'Sort 1';
 sort1Button.addEventListener('submit', (ev) => {
-  ev.preventdefault();
+  ev.preventDefault();
+  console.log('did it work?');
   const firstNum = bank.unshift();
   if (firstNum % 2 === 0) state.even.push(firstNum);
   else state.odd.push(firstNum);
 });
-
-
-
-
-const miniHeader2 = document.createElement('h2');
-miniHeader2.innerText = `
-  <h2>Odds</h2>
-  ${state.odd}
-`;
-
-const miniHeader3 = document.createElement('h2');
-miniHeader3.innerText = `
-  <h2>Evens</h2>
-  ${state.even}
-`;
-
-
-
-
-
-const returnForm = () => {
-  return userInputForm;
-}
-
-const oddOrEvenUpdate = () => {
-
-}
-
-const emptyBank = () => {
-  const but3 = document.createElement('button');
-  but2.innerText = 'Sort All';
-  but3.addEventListener('submit', (ev) => {
-    ev.preventdefault();
-    bank.forEach(num => {
-      if (num % 2 === 0) state.even.push(num);
-      else state.odd.push(num);
-    });
-  });
-  fo.appendChild(but3);
-}
-
+userInputForm.appendChild(sort1Button);
 console.log(state.bank);
 
 
+const bankHeader = document.createElement('h2');
+bankHeader.innerHTML = `
+  <h2>Bank</h2>
+  ${state.bank}
+`;
+lowerPart.appendChild(bankHeader);
 
+const oddsHeader = document.createElement('h2');
+oddsHeader.innerHTML = `
+  <h2>Odds</h2>
+  ${state.odd}
+`;
+lowerPart.appendChild(oddsHeader);
 
+const evensHeader = document.createElement('h2');
+evensHeader.innerHTML = `
+  <h2>Evens</h2>
+  ${state.even}
+`;
+lowerPart.appendChild(evensHeader);
 
-const everything = () => {
+//sortAll Button!!
+const sortAllButton = document.createElement('button');
+sortAllButton.innerText = 'Sort All';
+sortAllButton.addEventListener('submit', (ev) => {
+  ev.preventdefault();
+  console.log('did it work?');
+  bank.forEach(num => {
+    if (num % 2 === 0) state.even.push(num);
+    else state.odd.push(num);
+  });
+});
+userInputForm.appendChild(sortAllButton);
 
-
-
-
-};
-
+//RENDER
 const render = () => {
   const letsBegin = document.querySelector('#app');
   letsBegin.innerHTML = `
