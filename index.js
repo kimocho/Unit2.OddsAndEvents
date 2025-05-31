b2b2bf5b9d31915e5eb9174adba5da8de1605771
Samuel Kim <samjkim455@gmail.com>
2025-05-31 01:36:30 -0400

    think i'm done

--- index.js
+++ index.js
@@ -4,92 +4,65 @@ const state = {
   even: []
 }
 
-const sec = document.createElement('section');
-const lowerPart = document.createElement('section');
-sec.appendChild(lowerPart);
+const appDiv = document.querySelector('#app');
+appDiv.innerHTML = `
+  <h1>Odds and Evens</h1>
+  <form>
+    <label>Add a number to the banks</label><input />
+    <button id="addNum">Add number</button>
+    <button id="sort1">Sort 1</button>
+    <button id="sortAll">Sort All</button>
+  </form>
 
-//form onlyyyy
-const userInputForm = document.createElement('form');
-sec.appendChild(userInputForm);
-const myInput = document.createElement('input');
-const inputValue = myInput.value;
-userInputForm.appendChild(myInput);
-
-
-//add a number button
-const addNumberButton = document.createElement('button');
-addNumberButton.innerText = 'Add number';
-userInputForm.appendChild(addNumberButton);
-
-//form onlyyy
-addNumberButton.addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log('did it work?');
-  state.bank.push(inputValue);
-  console.log(state.bank);
-});
-
-
-
-
-
-//sort 1 button!!
-const sort1Button = document.createElement('button');
-sort1Button.innerText = 'Sort 1';
-sort1Button.addEventListener('submit', (ev) => {
-  ev.preventDefault();
-  console.log('did it work?');
-  const firstNum = bank.unshift();
-  if (firstNum % 2 === 0) state.even.push(firstNum);
-  else state.odd.push(firstNum);
-});
-userInputForm.appendChild(sort1Button);
-console.log(state.bank);
-
-
-const bankHeader = document.createElement('h2');
-bankHeader.innerHTML = `
   <h2>Bank</h2>
-  ${state.bank}
-`;
-lowerPart.appendChild(bankHeader);
+  <output id="bankOutput"></output>
 
-const oddsHeader = document.createElement('h2');
-oddsHeader.innerHTML = `
   <h2>Odds</h2>
-  ${state.odd}
-`;
-lowerPart.appendChild(oddsHeader);
+  <output id="oddsOutput"></output>
 
-const evensHeader = document.createElement('h2');
-evensHeader.innerHTML = `
   <h2>Evens</h2>
-  ${state.even}
+  <output id="evensOutput"></output>
 `;
-lowerPart.appendChild(evensHeader);
+const bankStorage = document.querySelector('#bankOutput');
+const oddsStorage = document.querySelector('#oddsOutput');
+const evensStorage = document.querySelector('#evensOutput');
 
-//sortAll Button!!
-const sortAllButton = document.createElement('button');
-sortAllButton.innerText = 'Sort All';
-sortAllButton.addEventListener('submit', (ev) => {
-  ev.preventdefault();
-  console.log('did it work?');
-  bank.forEach(num => {
-    if (num % 2 === 0) state.even.push(num);
-    else state.odd.push(num);
-  });
+const addNum = document.querySelector('#addNum');
+addNum.addEventListener('click', (event) => {
+  event.preventDefault();
+  const input = document.querySelector('input');
+  state.bank.push(input.value);
+  bankStorage.innerText = state.bank;
+  input.value = '';
 });
-userInputForm.appendChild(sortAllButton);
 
-//RENDER
-const render = () => {
-  const letsBegin = document.querySelector('#app');
-  letsBegin.innerHTML = `
-    <h1>Odds and Events</h1>
-    <h3>Add a number to the bank<h3>
-    <nonsense></nonsense>
-  `;
-  letsBegin.querySelector('nonsense').replaceWith(sec);
-}
+const sort1 = document.querySelector('#sort1');
+sort1.addEventListener('click', (event) => {
+  event.preventDefault();
+  const firstNum = state.bank.shift();
+  bankStorage.innerText = state.bank;
+  if (firstNum % 2 === 0) {
+    state.even.push(firstNum);
+    evensStorage.innerText = state.even;
+  }
+  else {
+    state.odd.push(firstNum);
+    oddsStorage.innerText = state.odd;
+  }
+});
 
-render();
+const sortAll = document.querySelector('#sortAll');
+sortAll.addEventListener('click', (event) => {
+  event.preventDefault();
+  state.bank.forEach(num => {
+    if (num % 2 === 0) {
+      state.even.push(num);
+      evensStorage.innerText = state.even;
+    }
+    else {
+      state.odd.push(num);
+      oddsStorage.innerText = state.odd;
+    }
+  })
+  bankStorage.innerText = "";
+});
